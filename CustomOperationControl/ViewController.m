970c71d97815cc8c22c5273f11bf829ddd578668
Jinwei <renjwjx@gmail.com>
2021-01-02 22:21:34 +0800

    operation control demo

--- CustomOperationControl/ViewController.m
+++ CustomOperationControl/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) NSOperationQueue* queue;
 @end
 
 @implementation ViewController
@@ -17,7 +17,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSOperationQueue* queue = [[NSOperationQueue alloc] init];
+    self.queue = queue;
+    queue.maxConcurrentOperationCount = 2;
 }
 
+- (IBAction)startBtn:(id)sender {
+    [self operationBlock];
+}
+
+- (IBAction)suspendBtn:(id)sender {
+    //只能suspend还没有开始的operation
+    [self.queue setSuspended:YES];
+}
+
+- (IBAction)continueBtn:(id)sender {
+    [self.queue setSuspended:NO];
+}
+
+- (IBAction)cancelBtn:(id)sender {
+    [self.queue cancelAllOperations];
+}
+
+- (void)operationBlock
+{
+    NSBlockOperation* block1 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 1000; i++) {
+            NSLog(@"block1 -%d-- %@", i, [NSThread currentThread]);
+        }
+    }];
+    NSBlockOperation* block2 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 1000; i++) {
+            NSLog(@"block2 -%d- %@", i, [NSThread currentThread]);
+        }
+    }];
+    NSBlockOperation* block3 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 1000; i++) {
+            NSLog(@"block3 -%d- %@", i, [NSThread currentThread]);
+        }
+    }];
+
+    [_queue addOperation:block1];
+    [_queue addOperation:block2];
+    [_queue addOperation:block3];
+    
+    [_queue addOperationWithBlock:^{
+                NSLog(@"add operation block-- %@", [NSThread currentThread]);
+    }];
+}
 
 @end
